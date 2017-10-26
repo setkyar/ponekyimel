f4d5bf19fc245581d0ed887aa3462b83ba1c1189
Set Kyar Wa Lar <setkyar16@gmail.com>
2017-10-26 12:04:38 +0630

    add analytic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,5 +142,15 @@
     
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108627209-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-108627209-1');
+        </script>
+
     </body>
 </html>
